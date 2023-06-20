4e8431d029e1568121a5b168af22395dd4716e18
MKJaakkola <73833749+MKJaakkola@users.noreply.github.com>
2023-06-20 20:33:28 +0300

    Update RunPAL_CaseStudies.R

--- RunPAL_CaseStudies.R
+++ RunPAL_CaseStudies.R
@@ -32,15 +32,15 @@ if(length(dropind) > 0) data = data[-dropind,]
 
 setwd(respath)
 
-# Run PAL with neutralizing for age and using time from seroconversion as the feature of interest
-pal_time_DAISY = PAL(data=data, info=info_DAISY, grouplabels="Labels", neutralise="Age", mainfeature="TimeFromSeroconversion", userandom="Donor", 
-                     neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", nodemin=1, seed=1234)
+# Run PAL with adjusting for age and using time from seroconversion as the feature of interest
+pal_time_DAISY = PAL(data=data, info=info_DAISY, grouplabels="Labels", adjust="Age", mainfeature="TimeFromSeroconversion", userandom="Donor", 
+                     adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", nodemin=1, seed=1234)
 write.table(pal_time_DAISY$pathwayscores, file="PAL_DAISY_NoAge_TimeFromSero.txt", sep="\t", quote=F)
 write.table(pal_time_DAISY$significance, file="PAL_DAISY_NoAge_TimeFromSero_fdr.txt", sep="\t", quote=F)
 
-# Run PAL with neutralizing for age and using sample groups as the feature of interest
-pal_group_DAISY = PAL(data=data, info=info_DAISY, grouplabels="Labels", neutralise="Age", mainfeature="SampleGroup", userandom="Donor", 
-                      neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", nodemin=1, seed=1234)
+# Run PAL with adjusting for age and using sample groups as the feature of interest
+pal_group_DAISY = PAL(data=data, info=info_DAISY, grouplabels="Labels", adjust="Age", mainfeature="SampleGroup", userandom="Donor", 
+                      adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", nodemin=1, seed=1234)
 write.table(pal_group_DAISY$pathwayscores, file="PAL_DAISY_NoAge_SampleGroup.txt", sep="\t", quote=F)
 write.table(pal_group_DAISY$significance, file="PAL_DAISY_NoAge_SampleGroup_fdr.txt", sep="\t", quote=F)
 
@@ -71,35 +71,35 @@ data_cd8 = data_cd8[pmax(apply(data_cd8[,caseind_cd8],1,median),apply(data_cd8[,
 
 setwd(respath)
 
-# Run PAL with neutralizing for age and using time from seroconversion as the feature of interest
-pal_time_pbmc = PAL(data=log2(data_pbmc+1), info=info_Diabimmune[colnames(data_pbmc),,drop=F], grouplabels="Label", neutralise="Age", mainfeature="TimeFromSero", 
-                    userandom="Donor", neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+# Run PAL with adjusting for age and using time from seroconversion as the feature of interest
+pal_time_pbmc = PAL(data=log2(data_pbmc+1), info=info_Diabimmune[colnames(data_pbmc),,drop=F], grouplabels="Label", adjust="Age", mainfeature="TimeFromSero", 
+                    userandom="Donor", adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_time_pbmc$pathwayscores, file="PAL_Diabimmune_PBMC_NoAge_TimeFromSero.txt", sep="\t", quote=F)
 write.table(pal_time_pbmc$significance, file="PAL_Diabimmune_PBMC_NoAge_TimeFromSero_fdr.txt", sep="\t", quote=F)
 
-pal_time_cd4 = PAL(log2(data_cd4+1), info=info_Diabimmune[colnames(data_cd4),,drop=F], grouplabels="Label", neutralise="Age", mainfeature="TimeFromSero", 
-                   userandom="Donor", neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+pal_time_cd4 = PAL(log2(data_cd4+1), info=info_Diabimmune[colnames(data_cd4),,drop=F], grouplabels="Label", adjust="Age", mainfeature="TimeFromSero", 
+                   userandom="Donor", adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_time_cd4$pathwayscores, file="PAL_Diabimmune_CD4_NoAge_TimeFromSero.txt", sep="\t", quote=F)
 write.table(pal_time_cd4$significance, file="PAL_Diabimmune_CD4_NoAge_TimeFromSero_fdr.txt", sep="\t", quote=F)
 
-pal_time_cd8 = PAL(log2(data_cd8+1), info=info_Diabimmune[colnames(data_cd8),,drop=F], grouplabels="Label", neutralise="Age", mainfeature="TimeFromSero", 
-                   userandom="Donor", neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+pal_time_cd8 = PAL(log2(data_cd8+1), info=info_Diabimmune[colnames(data_cd8),,drop=F], grouplabels="Label", adjust="Age", mainfeature="TimeFromSero", 
+                   userandom="Donor", adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_time_cd8$pathwayscores, file="PAL_Diabimmune_CD8_NoAge_TimeFromSero.txt", sep="\t", quote=F)
 write.table(pal_time_cd8$significance, file="PAL_Diabimmune_CD8_NoAge_TimeFromSero_fdr.txt", sep="\t", quote=F)
 
-# Run PAL with neutralizing for age and using samplegroup as the feature of interest
-pal_group_pbmc = PAL(log2(data_pbmc+1), info=info_Diabimmune[colnames(data_pbmc),,drop=F], grouplabels="Label", neutralise="Age", mainfeature="SampleGroup", 
-                     userandom="Donor", neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+# Run PAL with adjusting for age and using samplegroup as the feature of interest
+pal_group_pbmc = PAL(log2(data_pbmc+1), info=info_Diabimmune[colnames(data_pbmc),,drop=F], grouplabels="Label", adjust="Age", mainfeature="SampleGroup", 
+                     userandom="Donor", adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_group_pbmc$pathwayscores, file="PAL_Diabimmune_PBMC_NoAge_SampleGroup.txt", sep="\t", quote=F)
 write.table(pal_group_pbmc$significance, file="PAL_Diabimmune_PBMC_NoAge_SampleGroup_fdr.txt", sep="\t", quote=F)
 
-pal_group_cd4 = PAL(log2(data_cd4+1), info=info_Diabimmune[colnames(data_cd4),,drop=F], grouplabels="Label", neutralise="Age", mainfeature="SampleGroup", 
-                    userandom="Donor", neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+pal_group_cd4 = PAL(log2(data_cd4+1), info=info_Diabimmune[colnames(data_cd4),,drop=F], grouplabels="Label", adjust="Age", mainfeature="SampleGroup", 
+                    userandom="Donor", adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_group_cd4$pathwayscores, file="PAL_Diabimmune_CD4_NoAge_SampleGroup.txt", sep="\t", quote=F)
 write.table(pal_group_cd4$significance, file="PAL_Diabimmune_CD4_NoAge_SampleGroup_fdr.txt", sep="\t", quote=F)
 
-pal_group_cd8 = PAL(log2(data_cd8+1), info=info_Diabimmune[colnames(data_cd8),,drop=F], grouplabels="Label", neutralise="Age", mainfeature="SampleGroup", 
-                    userandom="Donor", neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+pal_group_cd8 = PAL(log2(data_cd8+1), info=info_Diabimmune[colnames(data_cd8),,drop=F], grouplabels="Label", adjust="Age", mainfeature="SampleGroup", 
+                    userandom="Donor", adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_group_cd8$pathwayscores, file="PAL_Diabimmune_CD8_NoAge_SampleGroup.txt", sep="\t", quote=F)
 write.table(pal_group_cd8$significance, file="PAL_Diabimmune_CD8_NoAge_SampleGroup_fdr.txt", sep="\t", quote=F)
 
@@ -132,15 +132,15 @@ data = data[,-dropindex]
 
 setwd(respath)
 
-# Run PAL with neutralizing for age and using time from serovonversion as the feature of interest
-pal_time_BabyDiet = PAL(data=data, info=info_BabyDiet, grouplabels="Labels", neutralise="Age", mainfeature="TimeFromSero", userandom="Donor", 
-                        neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+# Run PAL with adjusting for age and using time from serovonversion as the feature of interest
+pal_time_BabyDiet = PAL(data=data, info=info_BabyDiet, grouplabels="Labels", adjust="Age", mainfeature="TimeFromSero", userandom="Donor", 
+                        adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_time_BabyDiet$pathwayscores, file="PAL_BabyDiet_NoAge_TimeFromSero.txt", sep="\t", quote=F)
 write.table(pal_time_BabyDiet$significance, file="PAL_BabyDiet_NoAge_TimeFromSero_fdr.txt", sep="\t", quote=F)
 
-# Run PAL with neutralizing for age and using sample group as the feature of interest
-pal_group_BabyDiet = PAL(data=data, info=info_BabyDiet, grouplabels="Labels", neutralise="Age", mainfeature="SampleGroup", userandom="Donor", 
-                         neutralisationmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
+# Run PAL with adjusting for age and using sample group as the feature of interest
+pal_group_BabyDiet = PAL(data=data, info=info_BabyDiet, grouplabels="Labels", adjust="Age", mainfeature="SampleGroup", userandom="Donor", 
+                         adjustmentmodel="lmer", pathwaymodel="lmer", score="activity", seed=1234)
 write.table(pal_group_BabyDiet$pathwayscores, file="PAL_BabyDiet_NoAge_SampleGroup.txt", sep="\t", quote=F)
 write.table(pal_group_BabyDiet$significance, file="PAL_BabyDiet_NoAge_SampleGroup_fdr.txt", sep="\t", quote=F)
 
